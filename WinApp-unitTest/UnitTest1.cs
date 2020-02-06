542921dbcb6d38350717b18f062e4bb26ec2f77a
Karthicknadarajan Selvaraj <Karthicknadarajan.Selvaraj@kla-tencor.com>
2020-02-06 19:56:53 +0530

    Frst Comit

---
+++ WinApp-unitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WinApp_unitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+          [TestMethod]
+        public void TestMethod1()
+        {
+            ClassWinF Cls = new ClassWinF();
+           int result = Cls.Add();
+            Assert.AreEqual(10, result);
+        }
+    }
+}
