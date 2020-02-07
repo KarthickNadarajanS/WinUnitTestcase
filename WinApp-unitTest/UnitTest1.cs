51c9b42e3574b7f564b5de99459185af63f33507
Karthicknadarajan Selvaraj <Karthicknadarajan.Selvaraj@kla-tencor.com>
2020-02-07 14:26:52 +0530

    commt

--- WinApp-unitTest/UnitTest1.cs
+++ WinApp-unitTest/UnitTest1.cs
@@ -9,13 +9,13 @@ namespace WinApp_unitTest
     {
         
 
-          [TestMethod]
+        [TestMethod]
         public void TestMethod1()
         {
             //
-            ClassWinF Cls = new ClassWinF();
+           ClassWinF Cls = new ClassWinF();
            int result = Cls.Add();
-            Assert.AreEqual(10, result);
+           Assert.AreEqual(10, result);
         }
     }
 }
